eb0c8a4ad8af851702d3a37ddb8a32fb078d1145
Hack Reactor Student <student@hackreactor.com>
2018-06-11 14:44:57 -0700

    finished strigify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,27 +2,27 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+var str;
 var stringifyJSON = function(obj) {
+
 
 if(typeof obj === 'number'){
 console.log(obj.toString())
 return obj.toString();
 } else if (typeof obj === 'string'){
-console.log(obj)
 return  '\"' + obj + '\"';
 } else if(typeof obj === 'boolean'){
-console.log(obj)
 return obj.toString();
 } else if(obj === null){
-console.log(obj)
 return 'null';
 } else if(Array.isArray(obj)){
 return array(obj);
 } else if(typeof obj === 'object' && !Array.isArray(obj)){
  return object(obj);
 } else if(typeof obj === 'function'){
-return {};
+console.log('yup');
+str = 1;
+console.log(str)
 } 
 
 function array(ar){
@@ -32,6 +32,8 @@ for(var j = 0; j < ar.length; j++){
 arStr += stringifyJSON(ar[j]) + ',';
 }
 arStr = arStr.slice(0,arStr.length-1);
+if(str === 1){
+return '[]'}
 return '[' + arStr + ']';
 }
 
@@ -39,11 +41,15 @@ function object(obj1){
 var objStr = '';
 for(var prop in obj1){
 objStr += stringifyJSON(prop) + ':' + stringifyJSON(obj1[prop]) + ',';
-
 }
 objStr = objStr.slice(0,objStr.length-1);
 console.log(objStr);
+console.log(str);
+if(str === 1){
+return '{}'} else {
 return '{' + objStr + '}';
+}
+
 }
 
 };
